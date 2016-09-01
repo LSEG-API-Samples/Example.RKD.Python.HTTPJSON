a2e475884a1e6c9457059fcbc35591d80d80b848
Wasin Waeosri <wasin.waeosri@thomsonreuters.com>
2016-09-01 16:32:56 +0700

    add comments

--- trkd_authen.py
+++ trkd_authen.py
@@ -1,3 +1,13 @@
+'''
+The TRKD API sample code is provided for informational purposes only 
+and without knowledge or assumptions of the end users development environment. 
+We offer this code to provide developers practical and useful guidance while developing their own code. 
+However, we do not offer support and troubleshooting of issues that are related to the use of this code 
+in a particular environment; it is offered solely as sample code for guidance. 
+Please see the Thomson Reuters Knowledge Direct product page at http://customers.thomsonreuters.com 
+for additional information regarding the TRKD API.'''
+
+
 import os
 import sys
 import requests
@@ -9,10 +19,6 @@ username = raw_input('Please input username: ')
 ##use getpass.getpass to hide user inputted password
 password = getpass.getpass(prompt='Please input password: ')
 appid = raw_input('Please input appid: ')
-
-#username = 'trcsmnldauki@thomsonreuters.com'
-#appid = 'rkdapi'
-#password = 'Welcome75'
 
 print '############### Sending Authentication request message to TRKD ###############'
 
@@ -27,8 +33,10 @@ try:
     if result.status_code == 200:
         print 'Request success'
         print 'response status %s'%(result.status_code)
+        ##get Token
         token = result.json()['CreateServiceToken_Response_1']['Token']
         print 'Token: %s'%(token)
+        ##get expiraion
         expire = result.json()['CreateServiceToken_Response_1']['Expiration']
         print 'Exipre: %s'%(expire)
     elif result.status_code == 500:
